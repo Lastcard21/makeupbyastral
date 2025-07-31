3041022105b625d49d1951bfd7bf95770c666646
Akande Abdulquadir <akandeabdulquadirolamilekan@gmail.com>
2025-07-31 06:11:03 -0700

    the right link for thank you page

--- src/Pages/Booking.jsx
+++ src/Pages/Booking.jsx
@@ -92,7 +92,7 @@ function Booking() {
           <input type="hidden" name="_subject" value="New Booking Received!" />
           <input type="hidden" name="_template" value="table" />
           <input type="hidden" name="_captcha" value="false" />
-          <input type="hidden" name="_next" value="http:/https://makeupbyastral.netlify.app/thank-you" />
+          <input type="hidden" name="_next" value="https://makeupbyastral.netlify.app/thank-you" />
           <button
             type="submit"
             className="mt-6 w-full py-3 rounded-lg bg-darkred text-white font-semibold text-lg shadow-md hover:scale-105 transition-transform duration-200 border-2 border-darkred"
